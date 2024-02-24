1c52895d479f7482a6128da4520d7fd2c35c3358
Mitchelde <tretyakovanton00@mail.ru>
2024-02-24 15:36:34 +0300

    Timer work TODO: make timer more beautiful

--- lib/timer.dart
+++ lib/timer.dart
@@ -36,13 +36,27 @@ class _TimerAppState extends State<TimerApp> {
   @override
   void initState() {
     super.initState();
+    _timer = Timer(Duration.zero, () {});
+  }
+
+  bool isButtonVisible = true;
+
+  void _onButtonPressed(String buttonText) {
+    print('Button pressed with text: $buttonText');
+    // Here you can perform the necessary actions with the button text
+
+    // hide a button after clicking
+    setState(() {
+      isButtonVisible = buttonText == "1" ? false : true;
+    });
   }
 
   void startTimer() {
+    print('startTimer');
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         if (_hours == 0 && _minutes == 0 && _seconds == 0) {
-          _timer.cancel();
+          timer.cancel();
           // Timer has completed, you can perform any actions here
         } else if (_seconds > 0) {
           _seconds--;
@@ -58,26 +72,34 @@ class _TimerAppState extends State<TimerApp> {
     });
   }
 
+  void stopTimer() {
+    print('stopTimer');
+    if (_timer.isActive) {
+      print('stopTimer');
+      _timer.cancel();
+    }
+  }
+
   void resetTimer() {
     if (_timer.isActive) {
       _timer.cancel();
+    } else {
+      setState(() {
+        _hours = int.tryParse(_hoursController.text) ?? 0;
+        _minutes = int.tryParse(_minutesController.text) ?? 0;
+        _seconds = int.tryParse(_secondsController.text) ?? 0;
+
+        // Validate input ranges
+        _hours = _hours.clamp(0, 99);
+        _minutes = _minutes.clamp(0, 59);
+        _seconds = _seconds.clamp(0, 59);
+
+        // Update controllers to reflect clamped values
+        _hoursController.text = _hours.toString();
+        _minutesController.text = _minutes.toString();
+        _secondsController.text = _seconds.toString();
+      });
     }
-    setState(() {
-      _hours = int.tryParse(_hoursController.text) ?? 0;
-      _minutes = int.tryParse(_minutesController.text) ?? 0;
-      _seconds = int.tryParse(_secondsController.text) ?? 0;
-
-      // Validate input ranges
-      _hours = _hours.clamp(0, 99);
-      _minutes = _minutes.clamp(0, 59);
-      _seconds = _seconds.clamp(0, 59);
-
-      // Update controllers to reflect clamped values
-      _hoursController.text = _hours.toString();
-      _minutesController.text = _minutes.toString();
-      _secondsController.text = _seconds.toString();
-    });
-    startTimer();
   }
 
   @override
@@ -141,15 +163,56 @@ class _TimerAppState extends State<TimerApp> {
               ],
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: resetTimer,
-              child: const Text('Start Timer'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // ElevatedButton(
+                //   onPressed: stopTimer,
+                //   child: const Text('Stop Timer'),
+                // ),
+                // ElevatedButton(
+                //   onPressed: startTimer,
+                //   child: const Text('Start Timer'),
+                // ),
+                isButtonVisible
+                    ? ElevatedButton(
+                        onPressed: () {
+                          startTimer();
+                          _onButtonPressed("1");
+                        },
+                        child: const Text('Start Timer'),
+                      )
+                    : ElevatedButton(
+                        onPressed: () {
+                          stopTimer();
+                          _onButtonPressed("2");
+                        },
+                        child: const Text('Stop Timer'),
+                      ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    resetTimer();
+                    _onButtonPressed("2");
+                  },
+                  child: const Text('Reset Timer'),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
             Text(
               'Time remaining: $_hours hours $_minutes minutes $_seconds seconds',
               style: const TextStyle(fontSize: 24),
             ),
+            isButtonVisible
+                ? ElevatedButton(
+                    onPressed: () => _onButtonPressed('1'),
+                    child: Text('Кнопка 1'),
+                  )
+                : ElevatedButton(
+                    onPressed: () => _onButtonPressed('2'),
+                    child: Text('Кнопка 2'),
+                  ),
           ],
         ),
       ),
@@ -158,7 +221,9 @@ class _TimerAppState extends State<TimerApp> {
 
   @override
   void dispose() {
-    _timer.cancel(); // Cancel the timer when the widget is disposed
+    if (_timer.isActive) {
+      _timer.cancel();
+    }
     _hoursController.dispose();
     _minutesController.dispose();
     _secondsController.dispose();
